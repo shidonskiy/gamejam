8f4e5fd837123c9fad5c92faea9881bd55328cf2
Patryk Mazurek <patryk.mazurek@tensquaregames.com>
2022-05-13 23:26:21 +0200

    fixed jumping

--- gamejam_boost/Assets/GameJam/Scripts/Controllers/PlayerController.cs
+++ gamejam_boost/Assets/GameJam/Scripts/Controllers/PlayerController.cs
@@ -1,4 +1,7 @@
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
@@ -6,10 +9,15 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpVelocity;
     public float lerpTime;
-    public float wallDetectionDistance;
-    public float wallDetectionRadius;
+    public float floorDetectionDistance;
+    public float floorDetectionRadius;
+    public float sideWallDetectionDistance;
+    public float sideWallDetectionHeight;
+    public float sideWallDetectionWidth;
 
     public LayerMask groundLayer;
+    private bool _leftWall;
+    private bool _rightWall;
 
     private bool _floor;
 
@@ -31,9 +39,26 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _floor = Physics2D.OverlapCircle(transform.position + Vector3.down * wallDetectionDistance, wallDetectionRadius,
-            groundLayer);
+        _floor = Physics2D.OverlapCircle(transform.position + Vector3.down * floorDetectionDistance, floorDetectionRadius, groundLayer);
+        _leftWall = Physics2D.OverlapArea(
+            transform.position + Vector3.left * (sideWallDetectionDistance-sideWallDetectionWidth) + Vector3.up * sideWallDetectionHeight / 2 ,
+            transform.position + Vector3.left * (sideWallDetectionDistance+sideWallDetectionWidth) + Vector3.down * sideWallDetectionHeight / 2, groundLayer);
         
+        _rightWall = Physics2D.OverlapArea(
+            transform.position + Vector3.right * (sideWallDetectionDistance-sideWallDetectionWidth) + Vector3.up * sideWallDetectionHeight / 2 ,
+            transform.position + Vector3.right * (sideWallDetectionDistance+sideWallDetectionWidth) + Vector3.down * sideWallDetectionHeight / 2, groundLayer);
+
+        if (!_floor)
+        {
+            if (_leftWall)
+            {
+                _horizontal = Mathf.Max(_horizontal, 0f);
+            } else if (_rightWall)
+            {
+                _horizontal = Mathf.Min(_horizontal, 0f);
+            } 
+        }
+
         HorizontalMove(Mathf.Lerp(_rb.velocity.x, _horizontal * speed, lerpTime));
         
         if (_floor && _rb.velocity.y < 1e-3 && _jump)
@@ -62,6 +87,9 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
-        Gizmos.DrawWireSphere(transform.position + Vector3.down * wallDetectionDistance, wallDetectionRadius);
+        Gizmos.DrawWireSphere(transform.position + Vector3.down * floorDetectionDistance, floorDetectionRadius);
+        Gizmos.DrawWireCube(transform.position + Vector3.left * sideWallDetectionDistance, new Vector3(sideWallDetectionWidth, sideWallDetectionHeight, 0f));
+        Gizmos.DrawWireCube(transform.position + Vector3.right * sideWallDetectionDistance, new Vector3(sideWallDetectionWidth, sideWallDetectionHeight, 0f));
+
     }
 }
